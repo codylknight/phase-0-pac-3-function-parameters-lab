e1460dbdd0ad0132586d54c40e75df5c40ced470
cody knight <codylknight@icoud.com>
2023-04-05 18:17:18 -0500

    parametersLab

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(Name = "Aki") {
+    
+  return `Hi, my name is ${Name}.`
+
+
+}
+function introductionWithLanguage(Name = "Aki", language = "Ember.js"){
+  return `Hi, my name is ${Name} and I am learning to program in ${language}.`
+
+}
+function introductionWithLanguageOptional(Name = "Aki", language = "JavaScript"){
+    return `Hi, my name is ${Name} and I am learning to program in ${language}.`
+  
+  }
